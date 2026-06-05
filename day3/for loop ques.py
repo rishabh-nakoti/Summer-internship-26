16da4b39454cdd4da0a3545a8929a9cc1e1aeefe
Rishabh <rizshbh@gmail.com>
2026-06-05 11:48:41 +0530

    Add files via upload

---
+++ day3/for loop ques.py
@@ -0,0 +1,157 @@
+#37
+#123456789
+
+"""for i in range(1,10):
+    print(i,end=" ")
+print()
+
+
+# 2 4 6 8 10 12 14 16 18
+
+for i in range(1,10):
+    print(2*i,end=" ")
+print()
+
+
+#1 3 5 7 9 11 13
+    
+for i in range(0,7):
+    print(2*i+1,end=" ")
+print()
+
+
+#1 11 111 1111 11111 111111
+j=0
+for i in range(1,7):
+    j=j*10+1
+    print(j,end=" ")
+print()
+
+#1 10 100 1000 10000 100000
+j=1
+for i in range(1,7):
+    print(j,end=" ")
+    j=10*j
+print()
+
+
+# 1 4 9 16 25 36 49
+
+for i in range(1,8):
+    print(i*i,end=" ")
+
+
+# 2 4 8 16 32 64
+n=1
+for i in range(1,7):
+    n=n*2
+    print(n,end=" ")
+
+
+# 0 3 8 15 24 35
+for i in range(1,7):
+    print(i*i-1,end=" ")
+
+
+
+
+# 2 5 10 17 26 37
+for i in range(1,7):
+    print(i*i+1,end=" ")
+
+
+#0 1 1 2 3 5 8 13 (fibonacci)
+a=0
+b=1
+for i in range(1,9):
+    c=a+b
+    print(a,end=" ")
+    a=b
+    b=c
+print()
+
+# tribonacci
+a=0
+b=0
+c=1
+for i in range(1,9):
+    d=a+b+c
+    print(a,end=" ")
+    a=b
+    b=c
+    c=d
+
+
+
+#38 & 39
+n=int(input("enter a no. "))
+k=0
+for i in range(1,n+1):
+  if(n%i==0):
+    k=k+1
+if(k==2):
+  print("prime")
+else:
+  print("composite")
+
+
+
+#41
+n=int(input("enter a no. "))
+k=0
+for i in range(1,n):
+    if(n%i==0):
+        k=k+i
+if(k==n):
+    print("perfect no.")
+else:
+    print("not")
+
+
+
+#42
+a = int(input("Enter 1st no."))
+b = int(input("Enter 2nd no."))
+for i in range(a, a * b + 1):
+    if i % a == 0 and i % b == 0:
+        print("LCM =", i)
+        break"""
+
+
+
+
+#43
+a = int(input("Enter 1st no."))
+b = int(input("Enter 2nd no."))
+
+for i in range(a*b,0,-1):
+    if(a%i==0 and b%i==0):
+      print("HCF = ",i)
+      break
+
+
+
+#44
+for i in range(1,11):
+  k=0
+  for j in range(1,i+1):
+      if(i%j==0):
+        k=k+1
+      if(k==2):
+        print(i)
+
+
+
+#45
+for i in range(1,1001):
+  k=0
+  for j in range(1,i):
+      if(i%j==0):
+        k=k+j
+  if(k==i):
+      print(i)
+    
+
+
+
+    
---
+++ day4/while loop.py
@@ -0,0 +1,80 @@
+"""num=int(input("enter a no."))
+sum=0
+n=num
+while(num!=0):
+    r=num%10
+    sum=sum+r
+    num=num//10
+print(sum)
+
+
+
+#55 57
+num=1124
+sum=0
+mul=1
+while(num!=0):
+    r=num%10
+    sum=sum+r
+    mul=mul*r
+    num=num//10
+if(sum==mul):
+    print("spy")
+else:
+    print("not")
+
+
+
+#48
+num=153
+n=num
+sum=0
+while(num!=0):
+    r=num%10
+    cube=r*r*r
+    sum=sum+cube
+    num=num//10
+
+if(sum==n):
+    print("armstrong")
+else:
+    print("not armstrong")
+
+
+
+#53
+num=21
+n=num
+sum=0
+while(num!=0):
+    r=num%10
+    sum=sum+r
+    num=num//10
+if(n%sum==0):
+    print("niven")
+else:
+    print("not niven")"""
+
+
+
+#51
+n=9
+sq=n*n
+sum=0
+while(sq>0):
+    r=sq%10
+    sum=sum+r
+    sq=sq//10
+if(sum==n):
+    print("neon")
+else:
+    print("not neon")
+
+
+
+#52
+n=1729
+
+
+
+#
