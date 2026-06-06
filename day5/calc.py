bdbbf880fd6cf64eaf575725a483798d045a73b4
Rishabh <rizshbh@gmail.com>
2026-06-06 11:53:05 +0530

    Add files via upload

---
+++ day5/calc.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return(x+y)
+
+def sub(x,y):
+    return(x-y)
+
+def mul(x,y):
+    return(x*y)
+
+def div(x,y):
+    return(x/y)
+
+def km_m(x):
+    return(x*1000)
---
+++ day5/modules import.py
@@ -0,0 +1,17 @@
+from calc import *
+
+
+c=add(100,200)
+print(c)
+
+c=sub(100,200)
+print(c)
+
+c=mul(100,200)
+print(c)
+
+c=div(100,200)
+print(c)
+
+c=km_m(23)
+print(c)
