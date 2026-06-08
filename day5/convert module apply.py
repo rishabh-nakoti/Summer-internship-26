ba41d35f7cbd16739a69623ee73fecfb147394f0
Rishabh <rizshbh@gmail.com>
2026-06-09 00:02:15 +0530

    Add files via upload

---
+++ day5/convert module apply.py
@@ -0,0 +1,26 @@
+from convert import *
+
+
+print("7 km to m =",km_m(7),"m")
+
+print("5000 m to km =",m_km(5000),"km")
+
+print("100 F to C =",f_c(100),"C")
+
+print("37 C to F =",c_f(37),"F")
+
+print("2 hr to min =",hr_min(2),"min")  
+
+print("300 min to hr =",min_hr(300),"hr")
+
+print ("400 s to hr =",s_hr(400),"hr")
+
+print("4 hr to s =",hr_s(400),"s")
+
+print("3000 g to kg =",g_kg(3000),"kg")
+
+print("18 kg to g =",kg_g(18),"g")
+
+print("square of 7 =",sq(7))
+
+print("cube of 3 =",cube(3))
