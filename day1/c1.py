a757cd7b9f54bda74ab2f4e649760fca93a2fbd3
Rishabh <rizshbh@gmail.com>
2026-06-09 00:07:29 +0530

    Add files via upload

---
+++ day1/c1.py
@@ -0,0 +1,119 @@
+print("a = ")
+a=10
+print("b = ")
+b=200
+c=a+b
+print("The addition of a and b is ",c)
+
+print("x = ")
+x=478
+print("y = ")
+y=78
+z=x-y
+print("The subtraction of x and y is ", z)
+
+m=54
+n=5
+o=m*n
+print("The multiplication of m and n is ",o)
+
+i=89
+j=77
+k=i/j
+print("The division of i and j is ", k)
+
+r=5
+p=3.141
+a=p*r*r
+print("Area of circle is ",a)
+
+l=7
+b=10
+a=l*b
+print("Area of Rectangle is ", a)
+
+s=7
+a=s*s*s
+print("Area of Cube is ", a)
+
+l=5
+b=3
+h=8
+print("Area of Cubiod is ",a)
+
+hr=1
+mi=60
+htm= hr * mi
+print("Hours to Minutess ",htm)
+m=678
+mth=m/mi
+print("Minutes to Hours ",mth)
+
+
+hr=7
+s=3600
+hts=hr*s
+print("Hours to Seconds ",hts)
+ss=8980
+sth=ss/s
+print("Seconds to Hours",sth)
+
+km=65.7
+m=1000
+kmtm=km*m
+print("km to meter ", kmtm)
+
+mm=78798
+m=1000
+mtkm=mm/m
+print("Meter to km ", mtkm)
+
+h=7
+m=60
+s=60
+hms=h*m*s
+print("Hour Min Sec ", hms)
+
+s=4875389
+m=60
+h=60
+smh=s/m/h
+print("Sec Min Hour ",smh)
+
+c=78
+f=(c*9/5)+32
+print("C to F ", f)
+
+f=78
+c=(f-32)*5/9
+print("F to C ",c)
+
+a=10
+b=78
+temp=a
+a=b
+b=temp
+print(a)
+print(b)
+
+a=78
+b=100
+a,b=b,a
+print(a)
+print(b)
+
+a=65
+b=90
+print("a = ", a)
+print("b = ", b)
+a=a+b
+b=a-b
+a=a-b
+print("a = ",a)
+print("b = ",b)
+
+print("Enter a and b ")
+a=int(input("a = "))
+b=int(input("b = "))
+c=a+b
+print("a + b = ", c)
